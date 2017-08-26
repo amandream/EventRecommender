9553a3a18b3c62e2a06e0e6bcfc8af5ec5970d0c
mac <mac@macdembp-2.fios-router.home>
2017-08-26 17:10:32 -0400

    Initial Commit

---
+++ SummerYelpMock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SummerYelpMock
+//
+//  Created by mac on 2017/8/26.
+//  Copyright © 2017年 msw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
